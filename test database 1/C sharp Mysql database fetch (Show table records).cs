234e4014d16720a5b51b287ef56669c9ed06f2a5
gulsher.alam <gulsher.alam@GULSHERALAM-IT->
2023-03-13 23:20:42 +0530

    Add project files.

---
+++ test database 1/C sharp Mysql database fetch (Show table records).cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace test_database_1
+{
+    public partial class Form1 : Form
+    {
+        //Connection string
+
+        MySqlConnection con = new MySqlConnection("SERVER= LOCALHOST;DATABASE=news-site;UID=root;Password=");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MySqlDataAdapter da = new MySqlDataAdapter("SELECT * FROM user",con);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+
+        }
+    }
+}
